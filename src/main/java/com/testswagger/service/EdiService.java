ab4eb62e88c76d594af6eca1f39f2148d229ada1
Eric Wen <erichjw@A04M2120118.mitac.com.tw>
2024-12-30 16:42:40 +0800

    service improve

--- src/main/java/com/testswagger/service/EdiService.java
+++ src/main/java/com/testswagger/service/EdiService.java
@@ -13,6 +13,7 @@ public class EdiService {
 
     private final static String NEW_LINE = System.lineSeparator();
     private final static String PUSH_PNR = "MSG+:22'";
+    private final static String PNR_START_POINT = "SRC'";
     private static int msgRowCount = 0;
 
     public String createEdi(ItineraryDTO itineraryDTO) {
@@ -26,7 +27,7 @@ public class EdiService {
         edifactBuilder.append(wirteORG(itineraryDTO));
         edifactBuilder.append(wirteTVLLevel0(itineraryDTO));
         edifactBuilder.append(wirteEQNLevel0(itineraryDTO));
-        edifactBuilder.append(wirtePNRMessages(itineraryDTO.getPassengers()));
+        edifactBuilder.append(wirtePNRMessages(itineraryDTO));
         edifactBuilder.append(writeUNT(msgRowCount));
         edifactBuilder.append(writeUNE());
         edifactBuilder.append(writeUNZ());
@@ -102,22 +103,49 @@ public class EdiService {
     public String wirteEQNLevel0(ItineraryDTO itineraryDTO){
         StringBuilder sb = new StringBuilder();
         int passengersNum = itineraryDTO.getPassengers().size();
-        sb.append("EQN+" + passengersNum + "+'")
-                .append(lineBreak());
+        sb.append("EQN+" + passengersNum + "+'").append(lineBreak());
         return sb.toString();
     }
 
-    public String wirtePNRMessages(List<PassengersDTO> passengersDTOList){
+    public String wirtePNRMessages(ItineraryDTO itineraryDTO){
+        List<PassengersDTO> passengersDTOList = itineraryDTO.getPassengers();
+
         if (passengersDTOList.isEmpty())
             return lineBreak();
 
         StringBuilder sb = new StringBuilder();
         for (PassengersDTO passengersDTO : passengersDTOList) {
-
+            String onePNR = writeOnePNR(itineraryDTO, passengersDTO);
+            sb.append(onePNR);
         }
         return "";
     }
 
+    public String writeOnePNR(ItineraryDTO itineraryDTO,PassengersDTO passengersDTO){
+        StringBuilder sb = new StringBuilder();
+
+        String airlineCode = itineraryDTO.getAirlineCode();
+        String pnrNo = passengersDTO.getPnrRecordLocator();
+        String surname = passengersDTO.getSurname();
+        String firstName = passengersDTO.getFirstName();
+        String otherNames = passengersDTO.getOtherNames();
+        String refGR2 = "refGR2";
+
+        sb.append(PNR_START_POINT).append(lineBreak());
+        sb.append("RCI+"+ airlineCode + ":" + pnrNo +"'").append(lineBreak());
+        sb.append("ORG+"+ airlineCode + ":" + "USA" + "'").append(lineBreak()); //TODO "USA" 表訊息來源(e.g. 來源地, 機場, 航空公司總部...)
+        sb.append("ADD'").append(lineBreak()); //ADD段可不寫 表乘客地址、聯絡資訊
+        sb.append("TIF+"+ surname + "+" + firstName + ":A:" + refGR2 + "::" + otherNames + "'").append(lineBreak()); //乘客姓名, 稱謂, 種類
+        sb.append("FTI'").append(lineBreak()); //乘客在航空公司的識別碼
+        sb.append("REF'").append(lineBreak()); //向國家、政府單位提供的旅客參考編號, 不等於PNR No
+        sb.append("SSR+").append(lineBreak()); //特定旅客資訊
+
+
+
+
+
+        return sb.toString();
+    }
 
 
     // UNT UNE(optional) UNZ EDIFACT REF must be same with UNG(e.g. 283697159)
